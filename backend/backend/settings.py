d5cab9da08d9692e625463dfef0a28ef0a6cbafc
Muhammad Abbas <smabbasht>
2023-11-10 05:46:14 +0500

    do more changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,12 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static'), # You may need to adjust this path
-]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'build/static'), # You may need to adjust this path
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
