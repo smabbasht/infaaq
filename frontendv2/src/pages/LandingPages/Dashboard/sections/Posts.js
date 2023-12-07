3dd111f758c21d2cbc8b623d579f5a3d2a062a91
ProValarous <ahmedatif360@gmail.com>
2023-12-08 01:23:49 +0500

    frontendv2 updated

--- frontendv2/src/pages/LandingPages/Dashboard/sections/Posts.js
+++ frontendv2/src/pages/LandingPages/Dashboard/sections/Posts.js
@@ -13,6 +13,8 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+import React, { useState, useEffect } from "react";
+
 // @mui material components
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
@@ -32,7 +34,51 @@ import post2 from "assets/images/examples/testimonial-6-3.jpg";
 import post3 from "assets/images/examples/blog-9-4.jpg";
 import post4 from "assets/images/examples/blog2.jpg";
 
+const EventCard = (
+  title,
+  description,
+  date,
+  image //props: name, description, date
+) => {
+  const title_crop = title.slice(0, 10);
+  const desc = description.slice(0, 50);
+
+  return (
+    
+      <TransparentBlogCard
+        image={image}
+        title={title_crop}
+        description={desc}
+        // date={date}
+        action={{
+          type: "internal",
+          route: "/pages/blogs/author",
+          color: "info",
+          label: "read more",
+        }}
+      />
+  );
+};
+
 function Places() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(items.title);
+
   return (
     <MKBox component="section" py={2}>
       <Container>
@@ -40,8 +86,6 @@ function Places() {
           <MKTypography variant="h3" mb={6}>
             Events Happening
           </MKTypography>
-
-          
         </Grid>
         {/* <Grid container item xs={12}>
                 <MKButton variant="gradient" color="info" sx={{ height: "100%" }}>
@@ -49,7 +93,12 @@ function Places() {
                 </MKButton>
         </Grid> */}
         <Grid container spacing={3}>
-          <Grid item xs={12} sm={6} lg={3}>
+          {/* <Grid item xs={12} sm={6} lg={3}> */}
+            {items.map((item) => (
+              <Grid item xs={12} sm={6} lg={3}>
+                {EventCard(item.title, item.description, item.price, item.image)}
+              </Grid>
+            ))}
             <TransparentBlogCard
               image={post1}
               title="Rover raised $65 million"
@@ -62,110 +111,7 @@ function Places() {
               }}
             />
           </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post2}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "/pages/blogs/author",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "layouts/pages/authentication/sign-in",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "layouts/pages/authentication/sign-in",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "layouts/pages/authentication/sign-in",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "layouts/pages/authentication/sign-in",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "layouts/pages/authentication/sign-in",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6} lg={3}>
-            <TransparentBlogCard
-              image={post3}
-              title="MateLabs machine learning"
-              description="If you’ve ever wanted to train a machine learning model and integrate it with IFTTT, you now can with ..."
-              action={{
-                type: "internal",
-                route: "layouts/pages/authentication/sign-in",
-                color: "info",
-                label: "read more",
-              }}
-            />
-          </Grid>
-          {/* <Grid item xs={12} sm={6} lg={3}>
-            <BackgroundBlogCard
-              image={post4}
-              title="Flexible work hours"
-              description="Rather than worrying about switching offices every couple years, you stay in the same place."
-              action={{
-                type: "internal",
-                route: "/pages/blogs/author",
-                label: "read more",
-              }}
-            />
-          </Grid> */}
-        </Grid>
+        {/* </Grid> */}
       </Container>
     </MKBox>
   );
