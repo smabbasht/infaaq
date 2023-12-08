8c7aec41b7fe1179d493678faeb884574b289538
Muhammad Abbas <smabbasht>
2023-12-08 13:17:59 +0500

    remove 50 char limit in image text

--- backend/events/models.py
+++ backend/events/models.py
@@ -13,7 +13,7 @@ class Event(models.Model):
     n_attandees = models.IntegerField()
     raised_percentage = models.IntegerField()
     contact = models.IntegerField(unique=True)
-    image = models.CharField(max_length=50, default='default.png')
+    image = models.CharField(max_length=150, default='default.png')
 
     def __str__(self):
         return self.name
