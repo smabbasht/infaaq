de88735fb051e0553da3f3b34bfa1ee5edb8c081
ProValarous <ahmedatif360@gmail.com>
2023-12-08 13:20:42 +0500

    frontendv2 updated

--- frontendv2/src/pages/LandingPages/ShowEvent/index.js
+++ frontendv2/src/pages/LandingPages/ShowEvent/index.js
@@ -41,35 +41,7 @@ function ShowEvent() {
         // light
       />
       <MKBox bgColor="dark">
-        {/* <MKBox
-          minHeight="25rem"
-          width="100%"
-          sx={{
-            backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
-              `${linearGradient(
-                rgba(gradients.dark.main, 0.8),
-                rgba(gradients.dark.state, 0.8)
-              )}, url(${bgImage})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            display: "grid",
-            placeItems: "center",
-          }}
-        /> */}
-        {/* <Card
-          sx={{
-            p: 2,
-            mx: { xs: 2, lg: 3 },
-            mt: -8,
-            mb: 4,
-            backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
-            backdropFilter: "saturate(200%) blur(30px)",
-            boxShadow: ({ boxShadows: { xxl } }) => xxl,
-          }}
-        >
-          <Profile />
-          <Posts />
-        </Card> */}
+   
         <Contact />
         <Footer />
       </MKBox>
